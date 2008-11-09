c869db8cbc217d54c35d32855e44baedf607f8a3
unknown <matt@mcurry.net>
2008-11-09 00:03:17 -0500

    moved all cache files to sub-dir of webroot.

--- html_cache.php
+++ html_cache.php
@@ -4,6 +4,7 @@
  * Copyright (c) 2008 Matt Curry
  * www.PseudoCoder.com
  * http://github.com/mcurry/cakephp/tree/master/helpers/html_cache
+ * http://www.pseudocoder.com/archives/2008/09/03/cakephp-html-cache-helpercakephp-html-cache-helper/
  *
  * @author      mattc <matt@pseudocoder.com>
  * @license     MIT
@@ -12,9 +13,15 @@
 
 class HtmlCacheHelper extends Helper {
   function afterLayout() {
-    $view =& ClassRegistry::getObject('view');
-    $path = WWW_ROOT . implode(DS, array_filter(explode('/', $this->here)));
+    if(Configure::read('debug') > 0) {
+      return;
+    }
     
+    $view =& ClassRegistry::getObject('view');
+    $path = WWW_ROOT . 'cache' . DS
+            . implode(DS, array_filter(explode('/', $this->here)))
+            . DS . 'index.html';
+
     $file = new File($path, true);
     $file->write($view->output);
   }
