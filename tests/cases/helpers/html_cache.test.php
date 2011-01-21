49d24a0f3bcf248e04c13973b3a67e62bdfab1e7
AD7six <andydawson76@yahoo.co.uk>
2011-01-21 16:12:03 +0100

    whitespace

--- tests/cases/helpers/html_cache.test.php
+++ tests/cases/helpers/html_cache.test.php
@@ -3,65 +3,65 @@ App::import('Helper', array('HtmlCache.HtmlCache'));
 App::import('Core', array('View'));
 
 class HtmlCacheTestCase extends CakeTestCase {
-  public $View = null;
-  public $www_root = null;
+	public $View = null;
+	public $www_root = null;
 
-  public function startCase() {
-    $this->www_root = ROOT . DS . 'app' . DS . 'plugins' . DS . 'html_cache' . DS . 'tests' . DS . 'test_app' . DS . 'webroot' . DS;
-    $controller = null;
-    $this->View = new View($controller);
-    $this->View->loaded['HtmlCache'] = new HtmlCacheHelper(array('test_mode' => true, 'www_root' => $this->www_root));
-    $this->View->loaded['HtmlCache']->here = '/posts';
-  }
+	public function startCase() {
+		$this->www_root = ROOT . DS . 'app' . DS . 'plugins' . DS . 'html_cache' . DS . 'tests' . DS . 'test_app' . DS . 'webroot' . DS;
+		$controller = null;
+		$this->View = new View($controller);
+		$this->View->loaded['HtmlCache'] = new HtmlCacheHelper(array('test_mode' => true, 'www_root' => $this->www_root));
+		$this->View->loaded['HtmlCache']->here = '/posts';
+	}
 
-  public function endCase() {
-    $Folder = new Folder();
-    $Folder->delete($this->www_root . 'cache');
-  }
+	public function endCase() {
+		$Folder = new Folder();
+		$Folder->delete($this->www_root . 'cache');
+	}
 
-  public function testInstances() {
-    $this->assertTrue(is_a($this->View, 'View'));
-    $this->assertTrue(is_a(ClassRegistry::getObject('view'), 'View'));
-  }
+	public function testInstances() {
+		$this->assertTrue(is_a($this->View, 'View'));
+		$this->assertTrue(is_a(ClassRegistry::getObject('view'), 'View'));
+	}
 
-  public function testWriteCache() {
-    $expected = <<<END
+	public function testWriteCache() {
+		$expected = <<<END
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
 <html lang="en">
 <head>
-    <title>HtmlCache Test Title</title>
+	<title>HtmlCache Test Title</title>
 </head>
 <body>
-    HtmlCache Test Body
+	HtmlCache Test Body
 </body>
 </html>
 END;
 
-    $this->View->output = $expected;
-    $this->View->_triggerHelpers('afterLayout');
+		$this->View->output = $expected;
+		$this->View->_triggerHelpers('afterLayout');
 
-    $path = $this->www_root . 'cache' . DS . 'posts' . DS . 'index.html';
-    $this->assertTrue(file_exists($path));
-    $cached = file_get_contents($path);
-    $this->assertEqual($expected, $cached);
-  }
+		$path = $this->www_root . 'cache' . DS . 'posts' . DS . 'index.html';
+		$this->assertTrue(file_exists($path));
+		$cached = file_get_contents($path);
+		$this->assertEqual($expected, $cached);
+	}
 
-  public function testWriteNotHtmlCache() {
-	  $expected = <<<END
+	public function testWriteNotHtmlCache() {
+		$expected = <<<END
 <rss version="2.0">
 	<channel>
 		...
 	</channel>
 </rss>
 END;
-	  $this->View->loaded['HtmlCache']->here = '/posts.rss';
-	  $this->View->params['url']['ext'] = 'rss';
-	  $this->View->output = $expected;
+		$this->View->loaded['HtmlCache']->here = '/posts.rss';
+		$this->View->params['url']['ext'] = 'rss';
+		$this->View->output = $expected;
 
-	  $this->View->_triggerHelpers('afterLayout');
-	  $path = $this->www_root . 'cache' . DS . 'posts.rss';
-	  $this->assertTrue(file_exists($path));
-	  $cached = file_get_contents($path);
-	  $this->assertEqual($expected, $cached);
-  }
+		$this->View->_triggerHelpers('afterLayout');
+		$path = $this->www_root . 'cache' . DS . 'posts.rss';
+		$this->assertTrue(file_exists($path));
+		$cached = file_get_contents($path);
+		$this->assertEqual($expected, $cached);
+	}
 }
